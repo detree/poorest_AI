b3a3a00d981c2b5cfeba6fd0443281fdcd2217df
lunanli3 <lunanli3@illinois.edu>
2015-09-22 20:51:28 -0500

    maze_cell class progress

---
+++ prob1_searching/MP1_prob1/src/mp1_prob1/Maze_cell.java
@@ -0,0 +1,16 @@
+package mp1_prob1;
+
+public class Maze_cell {
+	private int index;
+	private int heuristic;
+	private int total_cost;
+	
+	public Maze_cell(){
+		index = -1;
+		heuristic = -1;
+		total_cost = -1;
+	}
+	
+	
+	
+}

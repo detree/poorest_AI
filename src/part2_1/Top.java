c379b77df4001dca672c37e12a44389cf93eb294
Shuchen Song <ssong18@illinois.edu>
2015-12-07 21:55:30 -0600

    small correction

--- src/part2_1/Top.java
+++ src/part2_1/Top.java
@@ -5,7 +5,7 @@ public class Top {
 
 	public static void main(String[] args) {
 		TrainSet train1=null;
-		train1 = new TrainSet("digitdata/testimages", "digitdata/testlabels");
+		train1 = new TrainSet("digitdata/trainingimages", "digitdata/traininglabels");
 		train1.scan_file();
 		
 		Perceptron p1;
@@ -15,7 +15,7 @@ public class Top {
 		p1.train_rand_order();
 		
 		Test tester;
-		tester = new Test(p1, "digitdata/trainingimages", "digitdata/traininglabels", 10);
+		tester = new Test(p1, "digitdata/testimages", "digitdata/testlabels", 10);
 		tester.scan_and_test();
 		tester.get_statistic();
 		System.out.println("size="+train1.get_size());
